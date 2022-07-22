b6c6e5d1ed6d2aba3b5821a79bc367c11e6bab20
zhaodongyu <stupid_zhaodongyu@hotmail.com>
2022-07-22 10:21:59 +0800

    test username

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -14,7 +14,7 @@ namespace WebApi
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = configuration; 
         }
 
         public IConfiguration Configuration { get; }
